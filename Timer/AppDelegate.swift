6e9858225321e233faeac867c0dd73478831ba51
Devian <info@devian.tk>
2015-04-29 17:24:21 +0200

    Initial Commit

---
+++ Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Damiaan Dufaux on 29/04/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Damiaan Dufaux on 29/04/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
